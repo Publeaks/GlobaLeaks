a4e45de7d238acfc6c8481c6fc64c379e597124a
Archien <damien_sjoer@hotmail.com>
2015-09-15 18:18:21 +0200

    Publeaks V2.0 Update

--- client/app/themes/publeaks/js/main.js
+++ client/app/themes/publeaks/js/main.js
@@ -1,45 +1,6 @@
 /* Main js file */
-$('.icon-security').click(function(){
-	$(this).toggleClass('Clicked-Step');
-	var ReadyToggleProgression = parseInt($('.loading-security p b').text());
-	if(ReadyToggleProgression == 100){
-		console.log('Equals 100% competion');
-		$('.security-loading-container').addClass('activate-upload-progression');
-	}else{
-		console.log('Does not equal 100% completion');
-	}
-});
-$('.icon-security').click(function(){
-	if($(this).hasClass('Progression-step')){
-		//do nothing
-	}else{
-		$(this).addClass('Progression-step')
-		var loadingStatus = parseInt($('.loading-security p b').text());
-		$('.loading-security p b').text(loadingStatus+25);
-		$('.loading-security span').css('width', loadingStatus + 25.40 + '%');
-	}
-});
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 800);
-        return false;
-      }
-    }
-  });
-});
-$('.main-nav a').click(function(){
-	$('.main-nav li').removeClass('active');
-	$(this).parent().addClass('active');
-});
-
-
+/**/
+/*
 (function($) {
  	$("#nav").click(function() {
 	$(".main-header").toggleClass("nav-focus");
@@ -51,218 +12,16 @@ $('.main-nav a').click(function(){
 				$('.main-header').removeClass('white-header');
 				
 			}, 500);
-			/*$("body").css("overflow", "hidden");
-			$("html.touch body").css("overflow", "hidden");*/
 		} else {
 			$("button#nav span").removeAttr("style");
 			$("button#nav span").toggleClass("white");
 			$(".update-screen").fadeToggle();
 			$('.main-header').addClass('white-header');
-			/*$("body").removeAttr("style");
-			$("html.touch body").removeAttr("style");*/
+
 		}
 	});           
 })(jQuery);
 
-var mainVideo = document.getElementById('main-intro-video');
-var PausedMainVideoTime;
-
-$('.video-overlay').click(function(){
-	$('.intro-video').addClass('video-active-focus');
-	$('body').css("overflow", "hidden");
-	$('.main-intro-video').prop('muted', false);
-	if($('.intro-video').hasClass('watched-video')){
-		
-		mainVideo.pause();
-	    mainVideo.currentTime = PausedMainVideoTime;
-	    mainVideo.play();
-		
-	}else{
-		mainVideo.pause();
-	    mainVideo.currentTime = '0';
-	    mainVideo.play();
-	}
-	$('.intro-video').addClass('watched-video');
-	
-	
-});
-
-$('.close-video-intro').click(function(){
-	$('.intro-video').removeClass('video-active-focus');
-	$('body').css("overflow", "auto");
-	$('.main-intro-video').prop('muted', true);
-	PausedMainVideoTime = mainVideo.currentTime;
-	console.log(PausedMainVideoTime);
-});
-$('.intro-video video').click(function(){
-	$('.intro-video').removeClass('video-active-focus');
-	$('body').css("overflow", "auto");
-	$('.main-intro-video').prop('muted', true);
-	PausedMainVideoTime = mainVideo.currentTime;
-	console.log(PausedMainVideoTime);
-});
-
-
-$('.icon-security').click(function(){
-	$('.icon-security').toggle();
-	$(this).toggle();
-	$(this).find('.security-popup-container').fadeToggle();
-	$(this).find('.security-popup-container').toggleClass('security-popup-container-active');
-	
-});
-
-/* Waypoints */
-
-
-
-$('.waarom-lekken').waypoint(function() {
-  	$('.main-nav ul li').removeClass('active');
-  	$('.organisatie-nav').addClass('active');
-    $('.main-header').removeClass('white-header');
-    $('.main-nav').removeClass('white-nav');
-    console.log('waarom-lekken-in-view');
-    
-    
-    
-    
-},{
-  offset:'0'
-});
-$('.security-section').waypoint(function() {
-  	$('.main-nav ul li').removeClass('active');
-  	$('.veiligheid-nav').addClass('active');
-    $('.main-header').removeClass('white-header');
-    $('.main-nav').removeClass('white-nav');
-    
-    
-    
-    
-},{
-  offset:'0'
-});
-$('.waarom-lekken').waypoint(function() {
-    var options = {
-	  useEasing : true, 
-	  useGrouping : true, 
-	  separator : ',', 
-	  decimal : '.', 
-	  prefix : '', 
-	  suffix : '' 
-	};
-	var WhyLeakCountUp = new CountUp("countUpWhyLeak", 0, 23, 0, 4, options);
-	WhyLeakCountUp.start();
-    this.destroy()
-    
-},{
-	offset:'0'
-});
-
-
-$('.organisation-section').waypoint(function() {
-  	
-    $('.main-nav ul li').removeClass('active');
-    $('.main-header').addClass('white-header');
-    $('.main-nav').addClass('white-nav');
-    $('.organisatie-nav').addClass('active');
-});
-$('.organisation-section').waypoint(function() {
-  	
-    $('.main-nav ul li').removeClass('active');
-    $('.main-header').addClass('white-header');
-    $('.main-nav').addClass('white-nav');
-    $('.organisatie-nav').addClass('active');
-});
-$('.intro-video').waypoint(function() {
-  	$('.main-nav ul li').removeClass('active');
-    $('.main-header').addClass('white-header');
-    $('.main-nav').addClass('white-nav');
-    $('.start-nav').addClass('active');
-    console.log('intro-video-in-view');
-});
-$('.waarom-zou-ik-delen').waypoint(function() {
-  	$('.main-nav ul li').removeClass('active');
-    $('.main-header').addClass('white-header');
-    $('.main-nav').addClass('white-nav');
-    $('.waarom-nav').addClass('active');
-    console.log('intro-video-in-view');
-});
-
-
-/* FORM HIDE AND SEEK & SUCH NONSENSE */
-$('.document-upload .stap-1-info i').click(function(){
-	$('.document-upload .stap-1-info').hide();
-	$('.collapse-form-content').addClass('show-first-content-form');
-});
-$('.section-2-form-1-1 input').click(function(){
-    if (this.checked) {
-        $('.section-2-form-1-2').css({
-        	'opacity': '1',
-        	'pointer-events': 'all'
-        });
-    }
-});
-$('.section-2-form-1-2 select').change(function(){
-	$('.section-2-form-1-3').css({
-        	'opacity': '1',
-        	'pointer-events': 'all'
-        });
-
-});
-$('.section-2-form-1-3 select').change(function(){
-	$('.section-2-form-1-4').css({
-        	'opacity': '1',
-        	'pointer-events': 'all'
-        });
-
-});
-$('.section-2-form-1-4 select').change(function(){
-	$('.progressie-button').css({
-	    	'opacity': '1',
-	    	'pointer-events': 'all'
-	});
-	if($(this).val() == 'Ja'){ // or this.value == 'volvo'
-		$('.section-2-form-1-5').css({
-		    	'display': 'block',
-		    	'opacity': '1',
-		    	'pointer-events': 'all'
-		});
-		$('.section-2-form-1-6').css({
-		    	'display': 'block',
-		    	'opacity': '1',
-		    	'pointer-events': 'all'
-		});
-	}
-  console.log($(this).val());
-});
-$('.first-form-part').click(function (){
-	$(this).replaceWith('<button class="progressie-button second-form-part">Volgende Stap</button>');
-	$('.second-form-part').css({
-	    	'opacity': '0.5',
-	    	'pointer-events': 'none'
-	});
-	$('.section-2-form-container').addClass('hidden-form-part');
-	$('.section-3-form-container').removeClass('hidden-form-part');
-	
-});
-$('.section-3-form-1-1 textarea').change(function(){
-	$('.progressie-button').css({
-	    	'opacity': '1',
-	    	'pointer-events': 'all'
-	});
-
-});
-$(document).on('click', '.second-form-part', function(){
-	
-	$(this).replaceWith('<button class="progressie-button third-form-part">Bevestig media keuze</button>');
-	$('.third-form-part').css({
-	    	'opacity': '0.5',
-	    	'pointer-events': 'none'
-	});
-	$('.section-3-form-container').addClass('hidden-form-part');
-	$('.section-4-form-container').removeClass('hidden-form-part');
-	console.log('second-form button beeing pushed');
-});
-/* pers keuze */
 var LandelijkeMediaKeuze = 3;
 
 $('.landelijke-pers li').click(function(){
@@ -349,6 +108,7 @@ $(document).on('click', '.third-form-part', function(){
 	console.log('third-form button beeing pushed');
 });
 
+/*
 
 
 
@@ -356,9 +116,162 @@ $(document).on('click', '.third-form-part', function(){
 
 
 
+/* new js */
+	var mainVideo = document.getElementById('main-intro-video');
+	var PausedMainVideoTime;
+
+$('.video-overlay').click(function(){
+	$('.intro-video').addClass('video-active-focus');
+	$('body').css("overflow", "hidden");
+	$('.main-intro-video').prop('muted', false);
+	if($('.intro-video').hasClass('watched-video')){
+		
+		mainVideo.pause();
+	    mainVideo.currentTime = PausedMainVideoTime;
+	    mainVideo.play();
+		
+	}else{
+		mainVideo.pause();
+	    mainVideo.currentTime = '0';
+	    mainVideo.play();
+	}
+	$('.intro-video').addClass('watched-video');
+	
+	
+});
+
+$('.close-video-intro').click(function(){
+	$('.intro-video').removeClass('video-active-focus');
+	$('body').css("overflow", "auto");
+	$('.main-intro-video').prop('muted', true);
+	PausedMainVideoTime = mainVideo.currentTime;
+	console.log(PausedMainVideoTime);
+});
+$('.intro-video video').click(function(){
+	$('.intro-video').removeClass('video-active-focus');
+	$('body').css("overflow", "auto");
+	$('.main-intro-video').prop('muted', true);
+	PausedMainVideoTime = mainVideo.currentTime;
+	console.log(PausedMainVideoTime);
+});
 
 
 
 
 
+$('.main-nav').stickThis({
+    top:        27,      // top position of sticky element, measured from 'ceiling'
+    minscreenwidth: 0,      // element will not be sticky when viewport width smaller than this
+    maxscreenwidth: 999999,     // element will not be sticky when viewport width larger than this 
+    zindex:     800,      // z-index value of sticky element
+    debugmode:  true   // when true, errors will be logged to console
+});
 
+
+
+
+$('.organisatie .dark-box').waypoint(function() {
+    var options = {
+	  useEasing : true, 
+	  useGrouping : true, 
+	  separator : ',', 
+	  decimal : '.', 
+	  prefix : '', 
+	  suffix : '' 
+	};
+	var WhyLeakCountUp = new CountUp("number-sprinter", 0, 40, 0, 4, options);
+	WhyLeakCountUp.start();
+    this.destroy()
+    
+},{
+	offset:'150'
+});
+
+$('.veiligheid .play-video').click(function(){
+	$(this).toggleClass('Clicked-Step');
+	var ReadyToggleProgression = parseInt($('.loading-security p b').text());
+	if(ReadyToggleProgression == 100){
+		console.log('Equals 100% competion');
+		$('.security-loading-container').addClass('activate-upload-progression');
+	}else{
+		console.log('Does not equal 100% completion');
+	}
+});
+$('.veiligheid .play-video').click(function(){
+	if($(this).hasClass('Progression-step')){
+		//do nothing
+	}else{
+		$(this).addClass('Progression-step')
+		var loadingStatus = parseInt($('.loading-security p b').text());
+		$('.loading-security p b').text(loadingStatus+25);
+		$('.loading-security span').css('width', loadingStatus + 25.40 + '%');
+	}
+});
+$('.main-nav a').click(function(){
+	$('.main-nav li').removeClass('active');
+	$(this).parent().addClass('active');
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
+$('.overlay-veiligheid-popup,.veiligheid-popup .close').click(function(){
+	$('.veiligheid-popup').fadeToggle();
+});
+
+
+
+
+/* CHECK TOR */
+/*
+  Copyright 2015 - Hermes Center - GlobaLeaks project
+  Author <giovanni.pellerano@evilaliv3.org>
+
+  Javascript CheckTor library
+*/	
+
+function checkTor(yes_cb, no_cb, test_url) {
+  // Test if the user is using Tor and in that case
+  // redirects the user to provided url
+  try {
+    if (typeof(test_url) === 'undefined') {
+      var test_url = 'https://antani.tor2web.org/checktor';
+    }
+    if (window.XMLHttpRequest) {
+      var xmlhttp = new XMLHttpRequest();
+
+      xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+          if (xmlhttp.getResponseHeader("x-check-tor") === "true") {
+            if (yes_cb) yes_cb();
+          } else {
+            if (no_cb) no_cb();
+          }
+        }
+      }
+
+      xmlhttp.open("GET", test_url, true);
+      xmlhttp.send();
+
+    }
+  } catch(err) {}
+}
+
+function redirectIfOnTor(url, test_url) {
+  var yes_cb = function () {
+    window.location.href = url;
+  }
+
+  checkTor(yes_cb, undefined, test_url);
+}

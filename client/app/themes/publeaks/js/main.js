f859d5a6e000f10b4bb29663fe9c20d74cbbd940
Archien <damien_sjoer@hotmail.com>
2015-09-16 20:29:46 +0200

    no message

--- client/app/themes/publeaks/js/main.js
+++ client/app/themes/publeaks/js/main.js
@@ -117,15 +117,18 @@ $(document).on('click', '.third-form-part', function(){
 
 
 /* new js */
+document.getElementById('main-intro-video').addEventListener('loadedmetadata', function() {
+	  this.currentTime = 11.014395;
+	}, false);
+
+
 $( document ).ready(function() {
     window.location = "/#/submission";
 
 
 
 
-	document.getElementById('main-intro-video').addEventListener('loadedmetadata', function() {
-	  this.currentTime = 11.014395;
-	}, false);
+	
 
 	var mainVideo = document.getElementById('main-intro-video');
 	var PausedMainVideoTime;

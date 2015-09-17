fff6f4fe363ecd729073bea84fe98ba4ed75e96b
Archien <damien_sjoer@hotmail.com>
2015-09-17 05:34:11 +0200

    no message

--- client/app/themes/publeaks/js/main.js
+++ client/app/themes/publeaks/js/main.js
@@ -295,51 +295,7 @@ $('.uploaden').waypoint(function() {
 	offset:'0',
 });
 
-
-
-/* CHECK TOR */
-/*
-  Copyright 2015 - Hermes Center - GlobaLeaks project
-  Author <giovanni.pellerano@evilaliv3.org>
-
-  Javascript CheckTor library
-*/	
-
-function checkTor(yes_cb, no_cb, test_url) {
-  // Test if the user is using Tor and in that case
-  // redirects the user to provided url
-  try {
-    if (typeof(test_url) === 'undefined') {
-      var test_url = 'https://antani.tor2web.org/checktor';
-    }
-    if (window.XMLHttpRequest) {
-      var xmlhttp = new XMLHttpRequest();
-
-      xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-          if (xmlhttp.getResponseHeader("x-check-tor") === "true") {
-            if (yes_cb) yes_cb();
-          } else {
-            if (no_cb) no_cb();
-          }
-        }
-      }
-
-      xmlhttp.open("GET", test_url, true);
-      xmlhttp.send();
-
-    }
-  } catch(err) {}
-}
-
-function redirectIfOnTor(url, test_url) {
-  var yes_cb = function () {
-    window.location.href = url;
-  }
-
-  checkTor(yes_cb, undefined, test_url);
-}
-$('#StepsNavBar a').click(function(){
+$('.pager a').click(function(){
 	
 	console.log('Form button clicked');
 	if ( $('#SubmissionTabsNavigationBox li:first-child').is( ".active" ) ) {
